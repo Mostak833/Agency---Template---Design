6bc882045fa243c446a59c2d8a53c74c9011657b
Gipumostak <90109815+gipumostak@users.noreply.github.com>
2021-12-31 01:22:47 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,48 @@
+//  >>>>>>>>>>>>>>>> for owl-carousel <<<<<<<<<<<<<<<<//
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      autoplay: true,
+      loop: true,
+      nav: true,
+    });
+  });
+//  >>>>>>>>>>>>>>>> for wow-animation <<<<<<<<<<<<<<<<//
 new WOW().init();
-
+//  >>>>>>>>>>>>>>>> for scroll-bar <<<<<<<<<<<<<<<<//
 AOS.init({
     duration:800,
 });
+//  >>>>>>>>>>>>>>>> for light-box <<<<<<<<<<<<<<<<//
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  })
+//  >>>>>>>>>>>>>>>> for slick-slider <<<<<<<<<<<<<<<<//
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    autoplay: true,
+    loop: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
